f64ba29192e1bea1be81ecccaddeef62d81474ab
Lohith R <lohith89@gmail.com>
2025-11-16 14:37:25 +0530

    first commit

--- server.py
+++ server.py
@@ -157,4 +157,5 @@ def predict_api():
 # RUN FLASK
 # -------------------------------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render dynamically sets PORT
+    app.run(host="0.0.0.0", port=port)
