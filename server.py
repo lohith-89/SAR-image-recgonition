f0f60ec659a1d4576bb2df25fd049b095b5e1b7a
Lohith R <lohith89@gmail.com>
2025-11-16 14:26:18 +0530

    first commit

--- server.py
+++ server.py
@@ -16,7 +16,9 @@ app.secret_key = "your_secret_key_here"
 # DOWNLOAD MODEL WEIGHTS FROM GOOGLE DRIVE IF MISSING
 # -------------------------------------------------
 weights_path = "downstream_model_weights.h5"
-drive_url = "https://drive.google.com/uc?export=download&id=1kxcFY-roQFFr3nw_uaEf2siW_XE3xWC4"
+drive_id = "1kxcFY‑roQFFr3nw_uaEf2siW_XE3xWC4"
+drive_url = f"https://drive.google.com/uc?id={drive_id}"
+
 
 if not os.path.exists(weights_path):
     print("\n🔽 Downloading model weights from Google Drive...")
